800650c1e151ea008fddd10ac33b77e16e2d88fc
howlettt <thomashowlett1@hotmail.com>
2014-05-11 10:51:17 -0600

    Adding Azure AD authentication

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express'),
-	settings = require('./settings'),
-	http = require('http'),
-	path = require('path'),
-	SocketHandler = require('./server/socket-handler'),
-	routeConfig = require('./server/route-config');
+    settings = require('./settings'),
+    http = require('http'),
+    path = require('path'),
+    SocketHandler = require('./server/socket-handler'),
+    routeConfig = require('./server/route-config'),
+    passport = require('passport'),
+    wsfedsaml2 = require('passport-azure-ad').WsfedStrategy; // This gives you WebSSO capability for Azure AD;
 
 var app = express();
 
@@ -18,6 +20,8 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // load liveReload script only in development mode
 // load before app.router
@@ -43,12 +47,77 @@ if ('development' == app.get('env')) {
 
 routeConfig.configureRoutes(app);
 
-var server = http.createServer(app).listen(app.get('port'), function(){
-  //console.log('Express server listening on port ' + app.get('port'));
+
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) { return next(); }
+	res.redirect('/login');
+}
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.
+passport.serializeUser(function (user, done) {
+	done(null, user.email);
+});
+passport.deserializeUser(function (id, done) {
+	findByEmail(id, function (err, user) {
+		done(err, user);
+	});
+});
+
+var wsfedStrategy = new wsfedsaml2({
+		realm: process.env.ActiveDirectoryRealm,
+		identityProviderUrl: process.env.ActiveDirectoryIdentityProviderUrl,
+		identityMetadata: process.env.ActiveDirectoryIdentityMetadata,
+		logoutUrl: process.env.ActiveDirectoryLogoutUrl
+	},
+    function(profile, done) {
+        if (!profile.email) {
+        	return done(new Error("No email found"), null);
+        }
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+        	findByEmail(profile.email, function(err, user) {
+        		if (err) {
+        			return done(err);
+        		}
+        		if (!user) {
+        			// "Auto-registration"
+        			users.push(profile);
+        			return done(null, profile);
+        		}
+        		return done(null, user);
+        	});
+        });
+    });
+
+passport.use(wsfedStrategy);
+
+var users = [];
+
+function findByEmail(email, fn) {
+	for (var i = 0, len = users.length; i < len; i++) {
+		var user = users[i];
+		if (user.email === email) {
+			return fn(null, user);
+		}
+	}
+	return fn(null, null);
+}
+
+
+var server = http.createServer(app).listen(app.get('port'), function () {
+	//console.log('Express server listening on port ' + app.get('port'));
 });
 
 var socketHandler = new SocketHandler(server);
-
-
 
 
--- server/route-config.js
+++ server/route-config.js
@@ -1,6 +1,7 @@
 var messages = require('./routes/messages'),
  chat = require('./routes/chat'),
- pages = require('./routes/pages');
+ pages = require('./routes/pages'),
+ passport = require('passport');
 
 exports.configureRoutes = function(app){
 
@@ -11,4 +12,25 @@ exports.configureRoutes = function(app){
 	app.get('/chat', chat.list);
 	app.post('/chat', chat.add);
 
+	app.get('/login', passport.authenticate('wsfed-saml2', { failureRedirect: '/', failureFlash: true }), function (req, res) {
+		res.redirect('/');
+	});
+	app.get('/logout', function (req, res) {
+		// clear the passport session cookies
+		req.logout();
+
+		// We need to redirect the user to the WSFED logout endpoint so the
+		// auth token will be revoked
+		wsfedStrategy.logout({}, function (err, url) {
+			if (err) {
+				res.redirect('/');
+			} else {
+				res.redirect(url);
+			}
+		});
+	});
+	app.post('/login/callback', passport.authenticate('wsfed-saml2', { failureRedirect: '/', failureFlash: true }),
+    function (req, res) {
+    	res.redirect('/');
+    });
 };
